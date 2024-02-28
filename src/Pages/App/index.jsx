d18880aba7af089f747ba97a1e647b17453b83c1
David Rivera A <davidriveraarbelaez@gmail.com>
2024-02-28 14:05:03 -0500

    Update index.jsx

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -2,12 +2,18 @@ import { useRoutes , BrowserRouter } from 'react-router-dom';
 import Navbar from '../../Components/Navbar'
 import Home from '../Home'
 import Asian from '../Asian'
+import Colombian from '../Colombian';
+import Sea from '../Sea';
 import './App.css'
+import FastFood from '../FastFood';
 
 const AppRoutes = () => {
   let routes = useRoutes([
     { path:'/', element:<Home />},
     { path: '/Asian', element: <Asian /> },
+    { path: '/fastFood', element: <FastFood /> },
+    { path: '/Sea', element: <Sea /> },
+    { path: '/Colombian', element: <Colombian /> },
   ])
   return routes
 }
